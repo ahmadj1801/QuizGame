e5fee07d5563af9080da9c71414be79196f572da
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2020-06-16 11:41:27 +0200

    Introduction to the game

---
+++ QuizGame/Game.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+inline void introduction();
+
+int main() {
+	introduction();
+	return 0;
+}
+
+void introduction() {
+	cout << "Welcome to *Insert Name*\n"
+		<< "\nHow to Play:\n"
+		<< "===============\n"
+		<< "   - The game is played by two players.\n"
+		<< "   - Each player will pick a topic from the list provided.\n"
+		<< "   - The questions generated for the quiz will be based on\n"
+		<< "     the topics choosen by the players.\n"
+		<< "   - Each player will be given two *perks?*\n"
+		<< "   - Correctly answered question will earn you one point, but\n"
+		<< "     string together X correct answers to earn yourself a\n"
+		<< "     score streak multiplier.\n"
+		<< "\nBelow is an example of a question you might encouter along with\n"
+		<< "its correct response:\n"
+		<< "Which country hosted the 2010 Fifa World Cup?\n"
+		<< "a) Spain\tb) Brazil\tc) South Africa\n"
+		<< "Expected response: c\n\n";
+
+}
